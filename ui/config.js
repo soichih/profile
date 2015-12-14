71a35c3151af9134a4069472d03624a1b6a902f6
Soichi Hayashi <hayashis@soichi7.ppa.iu.edu>
2015-12-14 17:04:26 +0000

    split config.js from index.html

--- ui/config.js
+++ ui/config.js
@@ -1,1 +1,23 @@
+'use strict';
+angular.module('app.config', []).constant('appconf', {
+    /*
+    icon_url: '../meshconfig/images/logo.png', 
+    home_url: '../meshconfig', 
+    */
+
+    //url base for sca-profile api
+    api: '../api/profile',
+
+    //shared servive api and ui urls (for menus and stuff)
+    shared_api: '../api/shared',
+    shared_url: '../shared',
+
+    //authentcation service API to refresh token, etc.
+    auth_api: '../api/auth',
+    //authentication service URL to jump if user needs to login
+    auth_url: '../auth',
+
+    jwt_id: 'jwt',
+});
+
 
