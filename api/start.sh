69cdabdc4426cab5a3b40a6d312f2f1b20dc0924
Soichi Hayashi <soichih@gmail.com>
2016-08-26 16:04:37 +0000

    renamed to sca-profile

--- api/start.sh
+++ api/start.sh
@@ -1,6 +1,6 @@
 #DEBUG=profile:* env=dev PORT=12402 nodemon -i node_modules ./index.js
 
-pm2 delete profile
-pm2 start profile.js --watch --ignore-watch="\.log$ \.sh$"
+pm2 delete sca-profile
+pm2 start profile.js --name sca-profile --watch --ignore-watch="\.log$ \.sh$"
 pm2 save
 #pm2 logs profile
