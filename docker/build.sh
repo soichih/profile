13ed239e5e795b2d75a5f518ef254a280e512f6a
Soichi Hayashi <hayashis@iu.edu>
2018-03-20 19:50:18 +0000

    moved apidoc to docs/

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,9 @@
-docker build -t soichih/sca-profile ..
+tag=1.1.0
+
+docker build -t soichih/profile ..
 if [ ! $? -eq 0 ]; then
     echo "failed to build"
     exit
 fi
-docker tag soichih/sca-profile soichih/sca-profile:1.0.0
-docker push soichih/sca-profile
+docker tag soichih/profile soichih/profile:$tag
+docker push soichih/profile:$tag
